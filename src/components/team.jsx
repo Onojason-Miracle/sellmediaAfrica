91962024471dafd3f6926cde9b2521a852bc0fc0
Onojason Miracle Ojonuyo <onojasonmiracle@gmail.com>
2024-06-27 22:38:55 +0100

    debug

--- src/components/team.jsx
+++ src/components/team.jsx
@@ -30,7 +30,7 @@ function Team() {
         <h1 className="teamText">Meet the heroes behind the magic</h1>
       </div>
 
-      <div className="team-flex">
+      {/* <div className="team-flex">
         {images.map((sellmedia) => (
           <div key={sellmedia.id} className="teamImage">
             <img
@@ -42,6 +42,15 @@ function Team() {
             <p className="text2">{sellmedia.text2}</p>
           </div>
         ))}
+      </div> */}
+
+      <div className="team-flex">
+        <div className="teamImage">
+        <img src="https://res.cloudinary.com/blackgirlmagic/image/upload/v1719518461/Contact%20Form/Avatar1_b1srar.png" alt="avatar1" />
+
+<p className="text1">Esther Howard</p>
+    <p className="text2">Founder</p>
+        </div>
       </div>
     </div>
   );
