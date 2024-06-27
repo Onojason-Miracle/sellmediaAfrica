f25aac61fcdc10eafdef00e1afc70a0a069cbfb3
Onojason Miracle Ojonuyo <onojasonmiracle@gmail.com>
2024-06-27 22:44:46 +0100

    -debugging

--- src/components/team.jsx
+++ src/components/team.jsx
@@ -1,28 +1,8 @@
 import React from "react";
-
+import images from "./imageText";
 
 function Team() {
-  const images = [
-    {
-      id: 1,
-      imageUrl: "https://res.cloudinary.com/blackgirlmagic/image/upload/v1719518461/Contact%20Form/Avatar1_b1srar.png",
-      text1: "Esther Howard",
-      text2: "Founder",
-    },
-    {
-      id: 2,
-      imageUrl: "https://res.cloudinary.com/blackgirlmagic/image/upload/v1719518461/Contact%20Form/Avatar2_ezjnwc.png",
-      text1: "Cody Fisher",
-      text2: "Developer",
-    },
-    {
-      id: 3,
-      imageUrl: "https://res.cloudinary.com/blackgirlmagic/image/upload/v1719518461/Contact%20Form/Avatar3_f2uxot.png",
-      text1: "Brooklyn Simmons",
-      text2: "Designer",
-    },
-  ];
-  
+
   
   return (
     <div id="team">
@@ -30,7 +10,7 @@ function Team() {
         <h1 className="teamText">Meet the heroes behind the magic</h1>
       </div>
 
-      {/* <div className="team-flex">
+      <div className="team-flex">
         {images.map((sellmedia) => (
           <div key={sellmedia.id} className="teamImage">
             <img
@@ -42,15 +22,6 @@ function Team() {
             <p className="text2">{sellmedia.text2}</p>
           </div>
         ))}
-      </div> */}
-
-      <div className="team-flex">
-        <div className="teamImage">
-        <img src="https://res.cloudinary.com/blackgirlmagic/image/upload/v1719518461/Contact%20Form/Avatar1_b1srar.png" alt="avatar1" />
-
-<p className="text1">Esther Howard</p>
-    <p className="text2">Founder</p>
-        </div>
       </div>
     </div>
   );
