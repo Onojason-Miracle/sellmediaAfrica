cb137609a43955f95b5d6f9bf66584f444a84457
Onojason Miracle Ojonuyo <onojasonmiracle@gmail.com>
2024-06-27 22:31:50 +0100

    team debugging

--- src/components/team.jsx
+++ src/components/team.jsx
@@ -1,7 +1,29 @@
 import React from "react";
-import images from "./imageText";
+
 
 function Team() {
+  const images = [
+    {
+      id: 1,
+      imageUrl: "https://res.cloudinary.com/blackgirlmagic/image/upload/v1719518461/Contact%20Form/Avatar1_b1srar.png",
+      text1: "Esther Howard",
+      text2: "Founder",
+    },
+    {
+      id: 2,
+      imageUrl: "https://res.cloudinary.com/blackgirlmagic/image/upload/v1719518461/Contact%20Form/Avatar2_ezjnwc.png",
+      text1: "Cody Fisher",
+      text2: "Developer",
+    },
+    {
+      id: 3,
+      imageUrl: "https://res.cloudinary.com/blackgirlmagic/image/upload/v1719518461/Contact%20Form/Avatar3_f2uxot.png",
+      text1: "Brooklyn Simmons",
+      text2: "Designer",
+    },
+  ];
+  
+  
   return (
     <div id="team">
       <div>
